ff55334491c02380b26aa98a1bbac38c43553c6b
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-12-22 09:13:18 +0100

    button works
    consol log show us what number we click

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-const buttons = document.querySelectorAll('button');
 const screenDisplay = document.querySelector('.screen');
+const buttons = document.querySelectorAll('button');
 
+
+
+
+function calculate(button) {
+    console.log(button)
+}
+
+buttons.forEach(button => button.addEventListener('click', () => calculate(button)) )
