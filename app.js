b8e3c3895107a3591d3aad19bf8e7ef7f5a949b7
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-12-22 11:05:55 +0100

    if else and eval method

--- app.js
+++ app.js
@@ -3,12 +3,27 @@ const buttons = document.querySelectorAll('button');
 
 
 let calculation =[];
+let accumulativeCalculation
 
 function calculate(button) {
    const value = button.textContent;
 
-   calculation.push(value)
-   console.log(calculation)
+   if(value == 'CLEAR') {
+    calculation = [];
+    screenDisplay.textContent = '.';
+   } else if ( value === '=') {
+    screenDisplay.textContent = eval(accumulativeCalculation)
+   } else {
+    calculation.push(value)
+    accumulativeCalculation = calculation.join('')
+    screenDisplay.textContent = accumulativeCalculation
+   }
+
+   
+
+
+
+   
 }
 
 buttons.forEach(button => button.addEventListener('click', () => calculate(button)) )
