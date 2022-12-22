ddeeb21690710e9cf2779c0ffa5bc7d3801e8e9f
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-12-22 09:38:55 +0100

    collecting all items in an array

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ let calculation =[];
 
 function calculate(button) {
    const value = button.textContent;
+
+   calculation.push(value)
+   console.log(calculation)
 }
 
 buttons.forEach(button => button.addEventListener('click', () => calculate(button)) )
